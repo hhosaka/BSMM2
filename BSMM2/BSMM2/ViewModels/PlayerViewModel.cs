3e01849684bb3a82ec354fc170a053f31998fc6e
hhosaka <hhosaka183@gmail.com>
2020-03-22 21:36:01 +0900

    Added files

---
+++ BSMM2/BSMM2/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BSMM2.Models;
+
+namespace BSMM2.ViewModels {
+
+	public class PlayerViewModel : BaseViewModel {
+		private BSMMApp _app;
+		public Player Player { get; }
+
+		public PlayerViewModel(BSMMApp app, Player player) {
+			_app = app;
+			Player = player;
+		}
+	}
+}
---
+++ BSMM2/BSMM2/Views/PlayerPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using BSMM2.Models;
+using BSMM2.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BSMM2.Views {
+
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PlayerPage : ContentPage {
+
+		public PlayerPage(BSMMApp app, Player player) {
+			InitializeComponent();
+			BindingContext = new PlayerViewModel(app, player);
+		}
+	}
+}
