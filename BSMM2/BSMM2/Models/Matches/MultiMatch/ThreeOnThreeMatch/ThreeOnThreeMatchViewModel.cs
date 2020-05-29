611822fc8290dedf1e1fd905cbcb817359aef7c6
hhosaka <hhosaka183@gmail.com>
2020-05-29 23:55:47 +0900

    ResultItem LifePoints

---
+++ BSMM2/BSMM2/Models/Matches/MultiMatch/ThreeOnThreeMatch/ThreeOnThreeMatchViewModel.cs
@@ -0,0 +1,68 @@
+﻿using BSMM2.ViewModels;
+using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace BSMM2.Models.Matches.MultiMatch.ThreeOnThreeMatch {
+
+	internal class ThreeOnThreeMatchViewModel : BaseViewModel {
+
+		internal class Item {
+			public string Label { get; }
+			public RESULT_T RESULT { get; }
+
+			public Item(string label, RESULT_T result) {
+				Label = label;
+				RESULT = result;
+			}
+		}
+
+		private Match _match;
+
+		public bool EnableLifePoint { get; }
+		public int LifePoint1 { get; set; }
+		public int LifePoint2 { get; set; }
+		public string LifePointTitle1 { get; }
+		public string LifePointTitle2 { get; }
+
+		private Action _update;
+
+		private Item _selectedItem;
+
+		public Item SelectedItem {
+			get => _selectedItem;
+			set {
+				SetProperty(ref _selectedItem, value);
+				_update?.Invoke();
+			}
+		}
+
+		public ObservableCollection<Item> Items { get; }
+
+		public ThreeOnThreeMatchViewModel(ThreeOnThreeMatchRule rule, Match match, Action back) {
+			_match = match;
+			EnableLifePoint = rule.EnableLifePoint;
+			var record1 = match.Record1;
+			var record2 = match.Record2;
+			var items = new ObservableCollection<Item>();
+			items.Add(new Item(record1.Player.Name + " Win", RESULT_T.Win));
+			items.Add(new Item("Draw", RESULT_T.Draw));
+			items.Add(new Item(record2.Player.Name + " Win", RESULT_T.Lose));
+			Items = items;
+			_update = Update;
+
+			LifePoint1 = record1.Result?.LifePoint ?? 0;
+			LifePointTitle1 = String.Format("{0}'s remaining Life Point", record1.Player.Name);
+			LifePoint2 = record2.Result?.LifePoint ?? 0;
+			LifePointTitle2 = String.Format("{0}'s remaining Life Point", record2.Player.Name);
+
+			void Update() {
+				if (SelectedItem != null) {
+					_match.SetResults(rule.CreatePoints(SelectedItem.RESULT));
+					MessagingCenter.Send<object>(this, Messages.REFRESH);
+					back?.Invoke();
+				}
+			}
+		}
+	}
+}
--- BSMM2/BSMM2/Models/Matches/SingleMatch/SingleMatchLPViewModel.cs
+++ BSMM2/BSMM2/Models/Matches/SingleMatch/SingleMatchLPViewModel.cs
@@ -1,60 +1,100 @@
 ﻿using BSMM2.ViewModels;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace BSMM2.Models.Matches.SingleMatch {
 
-	internal class SingleMatchLPViewModel : BaseViewModel {
-		private bool _player1;
+	using LifePoints = IEnumerable<LifePoint>;
 
-		public bool Player1 {
-			get => _player1;
+	internal class LifePoint {
+		private static LifePoints _instance;
+
+		public string Label { get; private set; }
+		public int? Point { get; private set; }
+
+		public static LifePoints Instance => GetInstance();
+
+		private static LifePoints GetInstance() {
+			if (_instance == null) {
+				_instance = new List<LifePoint> {
+					new LifePoint{ Label = "-", Point = null},
+					new LifePoint{ Label = "5", Point = 5},
+					new LifePoint{ Label = "4", Point = 4},
+					new LifePoint{ Label = "3", Point = 3},
+					new LifePoint{ Label = "2", Point = 2},
+					new LifePoint{ Label = "1", Point = 1},
+					new LifePoint{ Label = "0", Point = 0},
+				};
+			}
+			return _instance;
+		}
+
+		public static LifePoint GetItem(int? point)
+			=> GetInstance().First(lp => lp.Point == point);
+	}
+
+	internal class ResultItem {
+		private Action _onPropertyChanged;
+
+		public RESULT_T Value { get; private set; }
+
+		public ResultItem(Match match, Action onPropertyChanged) {
+			Initial(match.GetResult());
+			_onPropertyChanged = onPropertyChanged;
+		}
+
+		public bool Player1Win {
+			get => Value == RESULT_T.Win;
 			set {
-				if (value != _player1) {
-					Draw = Player2 = false;
-					SetProperty(ref _player1, value);
+				if (value != Player1Win) {
+					if (value) {
+						Draw = Player2Win = false;
+						Value = RESULT_T.Win;
+					} else {
+						Value = RESULT_T.Progress;
+					}
+					_onPropertyChanged?.Invoke();
 				}
 			}
 		}
-
-		private bool _draw;
 
 		public bool Draw {
-			get => _draw;
+			get => Value == RESULT_T.Draw;
 			set {
-				if (value != _draw) {
-					Player1 = Player2 = false;
-					SetProperty(ref _draw, value);
+				if (value != Draw) {
+					if (value) {
+						Player1Win = Player2Win = false;
+						Value = RESULT_T.Draw;
+					} else {
+						Value = RESULT_T.Progress;
+					}
+					_onPropertyChanged?.Invoke();
 				}
 			}
 		}
 
-		private bool _player2;
-
-		public bool Player2 {
-			get => _player2;
+		public bool Player2Win {
+			get => Value == RESULT_T.Lose;
 			set {
-				if (value != _player2) {
-					Draw = Player1 = false;
-					SetProperty(ref _player2, value);
+				if (value != Player2Win) {
+					if (value) {
+						Draw = Player1Win = false;
+						Value = RESULT_T.Lose;
+					} else {
+						Value = RESULT_T.Progress;
+					}
+					_onPropertyChanged?.Invoke();
 				}
 			}
 		}
 
-		public int Player1LP { get; set; }
-		public int Player2LP { get; set; }
-
-		public ICommand DoneCommand { get; }
-
-		public SingleMatchLPViewModel(SingleMatchRule rule, Match match, Action back) {
-			DoneCommand = new Command(Done);
-			var record1 = match.Record1;
-			var record2 = match.Record2;
-
-			switch (match.GetResult()) {
+		private void Initial(RESULT_T result) {
+			switch (result) {
 				case RESULT_T.Win:
-					Player1 = true;
+					Player1Win = true;
 					break;
 
 				case RESULT_T.Draw:
@@ -62,31 +102,43 @@ namespace BSMM2.Models.Matches.SingleMatch {
 					break;
 
 				case RESULT_T.Lose:
-					Player2 = true;
+					Player2Win = true;
 					break;
 			}
+		}
+	}
 
-			Player1LP = record1.Result.LifePoint;
-			Player2LP = record2.Result.LifePoint;
+	internal class SingleMatchLPViewModel : BaseViewModel {
+		public ResultItem ResultItem { get; }
+
+		private Match _match;
+
+		public LifePoint Player1LP { get; set; }
+		public LifePoint Player2LP { get; set; }
+		public IPlayer Player1 => _match.Record1.Player;
+		public IPlayer Player2 => _match.Record2.Player;
+
+		public LifePoints LifePoints
+			=> LifePoint.Instance;
+
+		public ICommand DoneCommand { get; }
+
+		public SingleMatchLPViewModel(SingleMatchRule rule, Match match, Action back) {
+			DoneCommand = new Command(Done);
+
+			_match = match;
+
+			Player1LP = LifePoint.GetItem(match.Record1.Result.LifePoint);
+			Player2LP = LifePoint.GetItem(match.Record2.Result.LifePoint);
+
+			ResultItem = new ResultItem(match, () => OnPropertyChanged(nameof(ResultItem)));
 
 			void Done() {
-				if (GetResult() != RESULT_T.Progress) {
-					match.SetResults(rule.CreatePoints(GetResult(), Player1LP, Player2LP));
+				if (ResultItem.Value != RESULT_T.Progress) {
+					match.SetResults(rule.CreatePoints(ResultItem.Value, Player1LP.Point ?? 0, Player2LP.Point ?? 0));// TODO tentativce
 					MessagingCenter.Send<object>(this, Messages.REFRESH);
 				}
 				back?.Invoke();
-			}
-
-			RESULT_T GetResult() {
-				if (Player1)
-					return RESULT_T.Win;
-				else if (Player2) {
-					return RESULT_T.Lose;
-				} else if (Draw) {
-					return RESULT_T.Draw;
-				} else {
-					return RESULT_T.Progress;
-				}
 			}
 		}
 	}
