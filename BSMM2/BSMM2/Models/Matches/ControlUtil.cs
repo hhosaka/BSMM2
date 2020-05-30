f2af49dacfdd30af985180faf0a4ed6ea4df0885
hhosaka <hhosaka183@gmail.com>
2020-05-30 11:12:06 +0900

    Refactoring took off ResultItems

--- BSMM2/BSMM2/Models/Matches/ControlUtil.cs
+++ BSMM2/BSMM2/Models/Matches/ControlUtil.cs
@@ -1,9 +1,112 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace BSMM2.Models.Matches {
 
 	using LIFEPOINTITEM_T = System.Collections.Generic.KeyValuePair<string, int?>;
+	using LifePoints = IEnumerable<LifePoint>;
 	using RESULTITEM_T = System.Collections.Generic.KeyValuePair<string, RESULT_T>;
+
+	internal class LifePoint {
+		private static LifePoints _instance;
+
+		public string Label { get; private set; }
+		public int? Point { get; private set; }
+
+		public static LifePoints Instance => GetInstance();
+
+		private static LifePoints GetInstance() {
+			if (_instance == null) {
+				_instance = new List<LifePoint> {
+					new LifePoint{ Label = "-", Point = null},
+					new LifePoint{ Label = "5", Point = 5},
+					new LifePoint{ Label = "4", Point = 4},
+					new LifePoint{ Label = "3", Point = 3},
+					new LifePoint{ Label = "2", Point = 2},
+					new LifePoint{ Label = "1", Point = 1},
+					new LifePoint{ Label = "0", Point = 0},
+				};
+			}
+			return _instance;
+		}
+
+		public static LifePoint GetItem(int? point)
+			=> GetInstance().First(lp => lp.Point == point);
+	}
+
+	internal class ResultItem {
+		private Action _onPropertyChanged;
+
+		public RESULT_T Value { get; private set; }
+
+		public ResultItem(Match match, Action onPropertyChanged) {
+			Initial(match.GetResult());
+			_onPropertyChanged = onPropertyChanged;
+		}
+
+		public bool Player1Win {
+			get => Value == RESULT_T.Win;
+			set {
+				if (value != Player1Win) {
+					if (value) {
+						Draw = Player2Win = false;
+						Value = RESULT_T.Win;
+					} else {
+						Value = RESULT_T.Progress;
+					}
+					_onPropertyChanged?.Invoke();
+				}
+			}
+		}
+
+		public bool Draw {
+			get => Value == RESULT_T.Draw;
+			set {
+				if (value != Draw) {
+					if (value) {
+						Player1Win = Player2Win = false;
+						Value = RESULT_T.Draw;
+					} else {
+						Value = RESULT_T.Progress;
+					}
+					_onPropertyChanged?.Invoke();
+				}
+			}
+		}
+
+		public bool Player2Win {
+			get => Value == RESULT_T.Lose;
+			set {
+				if (value != Player2Win) {
+					if (value) {
+						Draw = Player1Win = false;
+						Value = RESULT_T.Lose;
+					} else {
+						Value = RESULT_T.Progress;
+					}
+					_onPropertyChanged?.Invoke();
+				}
+			}
+		}
+
+		private void Initial(RESULT_T result) {
+			switch (result) {
+				case RESULT_T.Win:
+					Player1Win = true;
+					break;
+
+				case RESULT_T.Draw:
+					Draw = true;
+					break;
+
+				case RESULT_T.Lose:
+					Player2Win = true;
+					break;
+			}
+		}
+	}
 
 	internal class ControlUtil {
 
