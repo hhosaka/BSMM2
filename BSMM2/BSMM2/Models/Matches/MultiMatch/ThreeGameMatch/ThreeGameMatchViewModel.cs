575c5a105fc51077fa360f1e206697f145fc16df
hhosaka <hhosaka183@gmail.com>
2020-06-17 23:41:58 +0900

    Modified ThreeGameMatchViewModel

--- BSMM2/BSMM2/Models/Matches/MultiMatch/ThreeGameMatch/ThreeGameMatchViewModel.cs
+++ BSMM2/BSMM2/Models/Matches/MultiMatch/ThreeGameMatch/ThreeGameMatchViewModel.cs
@@ -28,15 +28,12 @@ namespace BSMM2.Models.Matches.MultiMatch.ThreeGameMatch {
 
 		public bool EnableLifePoint => _rule.EnableLifePoint;
 
-		private ResultItem[] _resultItems;
+		//private ResultItem[] _resultItems;
 
-		public ResultItem[] ResultItem {
-			get => _resultItems;
-			set => SetProperty<ResultItem[]>(ref _resultItems, value, nameof(ResultItem));
-		}
+		public ResultItem[] ResultItems { get; }
 
-		public LifePoint[] Player1LP { get; }
-		public LifePoint[] Player2LP { get; }
+		public LifePoint[] Player1LPs { get; }
+		public LifePoint[] Player2LPs { get; }
 		public IPlayer Player1 => _match.Record1.Player;
 		public IPlayer Player2 => _match.Record2.Player;
 
@@ -48,25 +45,18 @@ namespace BSMM2.Models.Matches.MultiMatch.ThreeGameMatch {
 			_match = match;
 			_rule = rule;
 			if (match.Record1.Result is MultiMatchResult result1) {
-				ResultItem = CreateItems();
-				Player1LP = CreateLifePoints(result1);
-				Player2LP = CreateLifePoints((MultiMatchResult)match.Record2.Result);
+				ResultItems = CreateItems();
+				Player1LPs = CreateLifePoints(result1);
+				Player2LPs = CreateLifePoints((MultiMatchResult)match.Record2.Result);
 			} else {
-				ResultItem = new[] {
-					new ResultItem(RESULT_T.Progress, () => OnPropertyChanged(nameof(ResultItem))),
-					new ResultItem(RESULT_T.Progress, () => OnPropertyChanged(nameof(ResultItem))),
-					new ResultItem(RESULT_T.Progress, () => OnPropertyChanged(nameof(ResultItem))),
-				};
-				Player1LP = Player2LP = new[]{
-					LifePoint.GetItem(-1),
-					LifePoint.GetItem(-1),
-					LifePoint.GetItem(-1),
-				};
+				var item = new ResultItem(RESULT_T.Progress, () => OnPropertyChanged(nameof(ResultItems)));
+				ResultItems = new[] { item, item, item };
+				Player1LPs = Player2LPs = new[] { LifePoint.GetItem(-1), LifePoint.GetItem(-1), LifePoint.GetItem(-1) };
 			}
 
 			ResultItem[] CreateItems() {
 				var items = new List<ResultItem>();
-				result1.Results.ForEach(result => items.Add(new ResultItem(result.RESULT, () => OnPropertyChanged(nameof(ResultItem)))));
+				result1.Results.ForEach(result => items.Add(new ResultItem(result.RESULT, () => OnPropertyChanged(nameof(ResultItems)))));
 				return items.ToArray();
 			}
 
@@ -77,9 +67,9 @@ namespace BSMM2.Models.Matches.MultiMatch.ThreeGameMatch {
 			}
 			void Done() {
 				match.SetMultiMatchResult(new[] {
-					new Score(ResultItem[0].Value, Player1LP[0].Point, Player2LP[0].Point),
-					new Score(ResultItem[1].Value, Player1LP[1].Point, Player2LP[1].Point),
-					new Score(ResultItem[2].Value, Player1LP[2].Point, Player2LP[2].Point),
+					new Score(ResultItems[0].Value, Player1LPs[0].Point, Player2LPs[0].Point),
+					new Score(ResultItems[1].Value, Player1LPs[1].Point, Player2LPs[1].Point),
+					new Score(ResultItems[2].Value, Player1LPs[2].Point, Player2LPs[2].Point),
 				}, EnableLifePoint);
 				MessagingCenter.Send<object>(this, Messages.REFRESH);
 				back?.Invoke();
